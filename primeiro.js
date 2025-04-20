81f2c46c2375ce3e5db62aaba7860e37fdf6786d
Guilherme_Pereira <guilherme098321@gmail.com>
2025-04-19 22:12:58 -0300

    html e css do verificador

---
+++ primeiro.js
@@ -0,0 +1,10 @@
+function validarNumero()
+{
+    //criar uma variavel para receber o valor digitado pelo usuario
+    let numero = document.getElementById("numero".value)
+    let mensagem = documento.getElementById("Mensagem")
+    
+    if (numero > 10) {
+        mensagem = "O número é maior que 10"
+    }
+}
