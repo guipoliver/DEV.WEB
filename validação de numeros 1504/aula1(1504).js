6bab2772b98321ac8dcc1cc1eb5580101d49d125
Guilherme Pereira de Oliveira <guilherme098321@gmail.com>
2025-04-29 14:30:58 -0300

    Add files via upload

---
+++ validação de numeros 1504/aula1(1504).js
@@ -0,0 +1,13 @@
+let x =  10
+let y = 3
+
+console.log('x = ' + x)
+console.log('y = ' + y)
+console.log('-x = ' + (-x))
+console.log('x/y = ' + (x/y))
+console.log('Resto de x por y = ' + (x%y))
+console.log('Inteiro de X por Y = ' + (Math.floor(x/y)))
+console.log('x + 1 = ' + (++x))
+console.log('x = ' + x)
+console.log('x + 1 = ' + (x++))
+console.log('x = ' + x)
---
+++ validação de numeros 1504/primeiro.js
@@ -0,0 +1,6 @@
+function validarNumero()
+{
+    //criar uma variavel para receber o valor digitado pelo usuario
+    let numero = document.getElementById("numero".value)
+    let mensagem = documento.getElementById("Mensagem")
+}
